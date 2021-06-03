e7904506f699e1b9459736c0493c12656b1d8ac9
fedeval <federico.valenza5@gmail.com>
2021-06-03 18:14:01 +0200

    feat: create custom order type and setup orderstore class

---
+++ src/models/order.ts
@@ -0,0 +1,12 @@
+import Client from '../database';
+
+export type Order = {
+  id?: number,
+  userId: number,
+  currentStatus: string
+}
+
+export class OrderStore {
+  // TODO: active order per user
+  // TODO: completed orders per user
+}
