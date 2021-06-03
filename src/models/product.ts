a9f017e778d932dfb4b384f53653fa1f2bc0e660
fedeval <federico.valenza5@gmail.com>
2021-06-03 18:08:58 +0200

    feat: create custom product type and set up productstore class

---
+++ src/models/product.ts
@@ -0,0 +1,15 @@
+import Client from '../database';
+
+export type Product = {
+  id?: number,
+  name: string,
+  price: number,
+  category: string
+}
+
+export class ProductStore {
+  // TODO: index action + category param (optional)
+  // TODO: show action
+  // TODO: create action
+  // TODO: topfive action
+}
