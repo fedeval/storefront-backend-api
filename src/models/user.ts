68b517351bea9378aacd379b3ca98a1b363b4b06
fedeval <federico.valenza5@gmail.com>
2021-06-03 18:11:47 +0200

    feat: create custom user type and setup userstore class

---
+++ src/models/user.ts
@@ -0,0 +1,15 @@
+import Client from '../database';
+
+export type User = {
+  id?: number,
+  username: string,
+  firstName: string,
+  lastName: string,
+  password: string
+}
+
+export class UserStore {
+  // TODO: index
+  // TODO: show
+  // TODO: create
+}
