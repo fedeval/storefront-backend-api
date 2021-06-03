a571be2aa1eb08691a9199f9b894e8a8c577bc6b
fedeval <federico.valenza5@gmail.com>
2021-06-03 17:05:14 +0200

    fix: dotenv usage for port, linting script

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import express, { Request, Response } from 'express'
-import dotenv from 'dotenv'
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
-const app: express.Application = express()
-const PORT: String | Number = process.env.PORT || 3000
+dotenv.config();
 
-app.use(express.json())
+const app: express.Application = express();
+const PORT: string | number = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(PORT, function () {
-    console.log(`starting app on port ${PORT}`)
-})
+  console.log(`starting app on port ${PORT}`);
+});
