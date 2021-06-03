34a96dc64654228d172f943dfb8bb12e07e3f313
fedeval <federico.valenza5@gmail.com>
2021-06-03 17:13:41 +0200

    feat: database connection file

---
+++ src/database.ts
@@ -0,0 +1,36 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config;
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSSWORD,
+  ENV
+} = process.env;
+
+console.log(ENV);
+let client = new Pool();
+
+if (ENV === 'dev') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSSWORD
+  });
+}
+
+if (ENV === 'test') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSSWORD
+  });
+}
+
+export default client;
