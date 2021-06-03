3a5eec9a712bca893e27e504d5f1b3aee0d8d90f
fedeval <federico.valenza5@gmail.com>
2021-06-03 16:53:34 +0200

    refactor: server file to use env variable for port

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const PORT: String | Number = process.env.PORT || 3000
 
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+app.listen(PORT, function () {
+    console.log(`starting app on port ${PORT}`)
 })
