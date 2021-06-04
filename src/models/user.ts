d11e8b3835401c79a864f84797664b39f956a2f9
fedeval <federico.valenza5@gmail.com>
2021-06-04 16:52:13 +0200

    feat: index, show and create methods for user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,8 @@
 import Client from '../database';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 export type User = {
   id?: number;
@@ -10,6 +14,43 @@ export type User = {
 
 export class UserStore {
   // TODO: index
+  async index(): Promise<User[]> {
+    try {
+      const connection = await Client.connect()
+      const sql = 'SELECT * FROM users;'
+      const result = await connection.query(sql)
+      connection.release()
+      return result.rows
+    } catch (err) {
+      throw new Error(`Cannot get users: ${err}`);
+    }
+  }
   // TODO: show
+  async show(id: number): Promise<User> {
+    try {
+      const connection = await Client.connect()
+      const sql = 'SELECT * FROM users WHERE id = ($1)'
+      const result = await connection.query(sql, [id])
+      const user = result.rows[0]
+      connection.release()
+      return user
+    } catch (err) {
+      throw new Error(`Cannot get user: ${err}`);
+    }
+  }
   // TODO: create
+  async create(user: User): Promise<User> {
+    try {
+      const connection = await Client.connect()
+      const sql = 'INSERT INTO users (username, first_name, last_name, password) VALUES ($1, $2, $3, $4)'
+      const hashedPassword = bcrypt.hashSync(user.password + process.env.PEPPER, parseInt(process.env.SALT_ROUNDS as unknown as string))
+      const result = await connection.query(sql, [user.username, user.firstName, user.lastName, hashedPassword])
+      const newUser = result.rows[0]
+      connection.release()
+      return newUser
+    } catch (err) {
+      throw new Error(`Cannot create user: ${err}`); 
+    }
+  }
+  // TODO: authenticate
 }
