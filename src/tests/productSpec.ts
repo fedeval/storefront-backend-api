aea9c6479b27ebb90d16d43dd140aa76da3463a6
fedeval <federico.valenza5@gmail.com>
2021-06-04 12:27:42 +0200

    test: added test for products crud actions

--- src/tests/productSpec.ts
+++ src/tests/productSpec.ts
@@ -1,6 +1,50 @@
 import { Product, ProductStore } from '../models/product';
 
 const store = new ProductStore()
+const productList: Product[] = [
+  {
+    name: 'bike',
+    price: 150,
+    category: 'sports',
+    rating: 4.3
+  },
+  {
+    name: 'kayak',
+    price: 600,
+    category: 'sports',
+    rating: 4.6
+  },
+  {
+    name: 'carpet',
+    price: 40,
+    category: 'household',
+    rating: 3.4
+  },
+  {
+    name: 'desk',
+    price: 200,
+    category: 'office',
+    rating: 3.9
+  },
+  {
+    name: 'pen',
+    price: 2,
+    category: 'office',
+    rating: 2.9
+  },
+  {
+    name: 'laptop',
+    price: 2000,
+    category: 'office',
+    rating: 4.9
+  },
+  {
+    name: 'chair',
+    price: 40,
+    category: 'household',
+    rating: 4.2
+  }
+]
 
 describe('Testing Product model', () => {
   it('Has an index method', () => {
@@ -15,4 +59,56 @@ describe('Testing Product model', () => {
   it('Has a topfive method', () => {
     expect(store.topfive).toBeDefined()
   })
+
+  beforeAll(() => {
+    productList.forEach((product) => store.create(product))
+  })
+
+  it('index should return a list of all products', async () => {
+    const result = await store.index()
+    expect(result).toEqual(productList)
+  })
+
+  it('create should add a product', async () => {
+    const result = await store.create({
+      name: 'notepad',
+      price: 9,
+      category: 'office',
+      rating: 4.2
+    })
+    expect(result).toEqual({
+      id: 8,
+      name: 'notepad',
+      price: 9,
+      category: 'office',
+      rating: 4.2
+    })
+  })
+
+  it('show should return the product with the given id', async () => {
+    const result = await store.show(8)
+    expect(result).toEqual({
+      id: 8,
+      name: 'notepad',
+      price: 9,
+      category: 'office',
+      rating: 4.2
+    })
+  })
+
+  it('delete should remove the product with the given id', async () => {
+    await store.delete(8)
+    const result = await store.index()
+    expect(result).not.toContain({
+      id: 8,
+      name: 'notepad',
+      price: 9,
+      category: 'office',
+      rating: 4.2
+    })
+  })
+
+  // TODO: test topfive
+  // TODO: test category
+
 })
