eaab5a31cd256782007dd6d602a6df6a7067c8d3
fedeval <federico.valenza5@gmail.com>
2021-06-04 12:03:59 +0200

    feat: add rating column to products table

--- build/tests/productSpec.js
+++ build/tests/productSpec.js
@@ -1,1 +1,18 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const product_1 = require("../models/product");
+const store = new product_1.ProductStore();
+describe('Testing Product model', () => {
+    it('Has an index method', () => {
+        expect(store.index).toBeDefined();
+    });
+    it('Has a show method', () => {
+        expect(store.show).toBeDefined();
+    });
+    it('Has a create method', () => {
+        expect(store.create).toBeDefined();
+    });
+    it('Has a topfive method', () => {
+        expect(store.topfive).toBeDefined();
+    });
+});
--- migrations/sqls/20210603152537-products-table-up.sql
+++ migrations/sqls/20210603152537-products-table-up.sql
@@ -2,5 +2,6 @@ CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   price INTEGER NOT NULL,
-  category VARCHAR(50) NOT NULL
+  category VARCHAR(50) NOT NULL,
+  rating NUMERIC(3,2) NOT NULL CONSTRAINT max_rating CHECK (rating <= 5)
 );
