8a2339280d67cdfb88dc136803ac572222ad2cda
fedeval <federico.valenza5@gmail.com>
2021-06-04 17:05:01 +0200

    feat: add authenticate method to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -49,8 +49,25 @@ export class UserStore {
       connection.release()
       return newUser
     } catch (err) {
-      throw new Error(`Cannot create user: ${err}`); 
+      throw new Error(`Cannot create user ${user.username}: ${err}`); 
     }
   }
   // TODO: authenticate
+  async authenticate(username: string, password: string): Promise<null | User> {
+    try {
+      const connection = await Client.connect()
+      const sql = 'SELECT * FROM users WHERE username=($1)'
+      const result = await connection.query(sql,[username])
+      let auth: null| User = null
+      if (result.rows.length) {
+        const user: User = result.rows[0]
+        if (bcrypt.compareSync(password + process.env.PEPPER, user.password)) {
+          auth = user
+        }
+      }
+      return auth
+    } catch (err) {
+      throw new Error(`Cannot authenticate user ${username}: ${err}`);
+    }
+  }
 }
