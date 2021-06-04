204f6e638f097c6aecc63e085736cf904537360f
fedeval <federico.valenza5@gmail.com>
2021-06-04 18:17:05 +0200

    style: formatting and naming improvements for readability in user.ts

--- build/models/user.js
+++ build/models/user.js
@@ -8,6 +8,20 @@ const database_1 = __importDefault(require("../database"));
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+// SQL and JS have different naming conventions:
+// - lower_snake_case: SQL columns
+// - camelCase: JS object properties
+// To avoid breaking conventions we can use this function to convert from SQL naming to JS when importing data from the DB
+const convertColNamesToUserProps = (id, username, first_name, last_name, password) => {
+    const user = {
+        id: id,
+        username: username,
+        firstName: first_name,
+        lastName: last_name,
+        password: password
+    };
+    return user;
+};
 class UserStore {
     // TODO: index
     async index() {
@@ -17,21 +31,23 @@ class UserStore {
             const result = await connection.query(sql);
             connection.release();
             console.log(typeof result.rows[0]);
-            return result.rows;
+            return result.rows.map((dbData) => {
+                return convertColNamesToUserProps(dbData.id, dbData.username, dbData.first_name, dbData.last_name, dbData.password);
+            });
         }
         catch (err) {
             throw new Error(`Cannot get users: ${err}`);
         }
     }
     // TODO: show
-    async show(id) {
+    async show(userId) {
         try {
             const connection = await database_1.default.connect();
-            const sql = 'SELECT * FROM users WHERE id = ($1)';
-            const result = await connection.query(sql, [id]);
-            const user = result.rows[0];
+            const sql = 'SELECT * FROM users WHERE id = ($1);';
+            const result = await connection.query(sql, [userId]);
+            const { id, username, first_name, last_name, password } = result.rows[0];
             connection.release();
-            return user;
+            return convertColNamesToUserProps(id, username, first_name, last_name, password);
         }
         catch (err) {
             throw new Error(`Cannot get user: ${err}`);
@@ -41,27 +57,28 @@ class UserStore {
     async create(user) {
         try {
             const connection = await database_1.default.connect();
-            const sql = 'INSERT INTO users (username, first_name, last_name, password) VALUES ($1, $2, $3, $4)';
+            const sql = 'INSERT INTO users (username, first_name, last_name, password) VALUES ($1, $2, $3, $4) RETURNING *;';
             const hashedPassword = bcrypt_1.default.hashSync(user.password + process.env.PEPPER, parseInt(process.env.SALT_ROUNDS));
             const result = await connection.query(sql, [user.username, user.firstName, user.lastName, hashedPassword]);
-            const newUser = result.rows[0];
+            const { id, username, first_name, last_name, password } = result.rows[0];
             connection.release();
-            return newUser;
+            return convertColNamesToUserProps(id, username, first_name, last_name, password);
         }
         catch (err) {
             throw new Error(`Cannot create user ${user.username}: ${err}`);
         }
     }
     // TODO: authenticate
-    async authenticate(username, password) {
+    async authenticate(username, passwordString) {
         try {
             const connection = await database_1.default.connect();
-            const sql = 'SELECT * FROM users WHERE username=($1)';
+            const sql = 'SELECT * FROM users WHERE username=($1);';
             const result = await connection.query(sql, [username]);
             let auth = null;
             if (result.rows.length) {
-                const user = result.rows[0];
-                if (bcrypt_1.default.compareSync(password + process.env.PEPPER, user.password)) {
+                const { id, username, first_name, last_name, password } = result.rows[0];
+                const user = convertColNamesToUserProps(id, username, first_name, last_name, password);
+                if (bcrypt_1.default.compareSync(passwordString + process.env.PEPPER, user.password)) {
                     auth = user;
                 }
             }
--- src/models/user.ts
+++ src/models/user.ts
@@ -2,7 +2,9 @@ import Client from '../database';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
+
+const { PEPPER, SALT_ROUNDS } = process.env;
 
 export type User = {
   id?: number;
@@ -16,42 +18,60 @@ export type User = {
 // - lower_snake_case: SQL columns
 // - camelCase: JS object properties
 // To avoid breaking conventions we can use this function to convert from SQL naming to JS when importing data from the DB
-const convertColNamesToUserProps = (id: number, username: string, first_name: string, last_name: string, password: string): User => {
+const convertColNamesToUserProps = (
+  id: number,
+  username: string,
+  first_name: string,
+  last_name: string,
+  password: string
+): User => {
   const user: User = {
     id: id,
     username: username,
     firstName: first_name,
     lastName: last_name,
     password: password
-  }
-  return user
-}
+  };
+  return user;
+};
 
 export class UserStore {
   // TODO: index
   async index(): Promise<User[]> {
     try {
-      const connection = await Client.connect()
-      const sql = 'SELECT * FROM users;'
-      const result = await connection.query(sql)
-      connection.release()
-      console.log(typeof result.rows[0])
+      const connection = await Client.connect();
+      const sql = 'SELECT * FROM users;';
+      const result = await connection.query(sql);
+      connection.release();
+      console.log(typeof result.rows[0]);
       return result.rows.map((dbData) => {
-        return convertColNamesToUserProps(dbData.id, dbData.username, dbData.first_name, dbData.last_name, dbData.password)
-      })
+        return convertColNamesToUserProps(
+          dbData.id,
+          dbData.username,
+          dbData.first_name,
+          dbData.last_name,
+          dbData.password
+        );
+      });
     } catch (err) {
       throw new Error(`Cannot get users: ${err}`);
     }
   }
   // TODO: show
-  async show(id: number): Promise<User> {
+  async show(userId: number): Promise<User> {
     try {
-      const connection = await Client.connect()
-      const sql = 'SELECT * FROM users WHERE id = ($1)'
-      const result = await connection.query(sql, [id])
-      const { dbId, username, first_name, last_name, password } = result.rows[0]
-      connection.release()
-      return convertColNamesToUserProps(dbId, username, first_name, last_name, password)
+      const connection = await Client.connect();
+      const sql = 'SELECT * FROM users WHERE id = ($1);';
+      const result = await connection.query(sql, [userId]);
+      const { id, username, first_name, last_name, password } = result.rows[0];
+      connection.release();
+      return convertColNamesToUserProps(
+        id,
+        username,
+        first_name,
+        last_name,
+        password
+      );
     } catch (err) {
       throw new Error(`Cannot get user: ${err}`);
     }
@@ -59,32 +79,57 @@ export class UserStore {
   // TODO: create
   async create(user: User): Promise<User> {
     try {
-      const connection = await Client.connect()
-      const sql = 'INSERT INTO users (username, first_name, last_name, password) VALUES ($1, $2, $3, $4)'
-      const hashedPassword = bcrypt.hashSync(user.password + process.env.PEPPER, parseInt(process.env.SALT_ROUNDS as unknown as string))
-      const result = await connection.query(sql, [user.username, user.firstName, user.lastName, hashedPassword])
-      const { dbId, username, first_name, last_name, password } = result.rows[0]
-      connection.release()
-      return convertColNamesToUserProps(dbId, username, first_name, last_name, password)
+      const connection = await Client.connect();
+      const sql =
+        'INSERT INTO users (username, first_name, last_name, password) VALUES ($1, $2, $3, $4) RETURNING *;';
+      const hashedPassword = bcrypt.hashSync(
+        user.password + PEPPER,
+        parseInt(SALT_ROUNDS as unknown as string)
+      );
+      const result = await connection.query(sql, [
+        user.username,
+        user.firstName,
+        user.lastName,
+        hashedPassword
+      ]);
+      const { id, username, first_name, last_name, password } = result.rows[0];
+      connection.release();
+      return convertColNamesToUserProps(
+        id,
+        username,
+        first_name,
+        last_name,
+        password
+      );
     } catch (err) {
-      throw new Error(`Cannot create user ${user.username}: ${err}`); 
+      throw new Error(`Cannot create user ${user.username}: ${err}`);
     }
   }
   // TODO: authenticate
-  async authenticate(username: string, password: string): Promise<null | User> {
+  async authenticate(
+    username: string,
+    pwdString: string
+  ): Promise<null | User> {
     try {
-      const connection = await Client.connect()
-      const sql = 'SELECT * FROM users WHERE username=($1)'
-      const result = await connection.query(sql,[username])
-      let auth: null| User = null
+      const connection = await Client.connect();
+      const sql = 'SELECT * FROM users WHERE username=($1);';
+      const result = await connection.query(sql, [username]);
+      let auth: null | User = null;
       if (result.rows.length) {
-        const { dbId, username, first_name, last_name, dbPassword } = result.rows[0]
-        const user: User = convertColNamesToUserProps(dbId, username, first_name, last_name, dbPassword)
-        if (bcrypt.compareSync(password + process.env.PEPPER, user.password)) {
-          auth = user
+        const { id, username, first_name, last_name, password } =
+          result.rows[0];
+        const user: User = convertColNamesToUserProps(
+          id,
+          username,
+          first_name,
+          last_name,
+          password
+        );
+        if (bcrypt.compareSync(pwdString + PEPPER, user.password)) {
+          auth = user;
         }
       }
-      return auth
+      return auth;
     } catch (err) {
       throw new Error(`Cannot authenticate user ${username}: ${err}`);
     }
