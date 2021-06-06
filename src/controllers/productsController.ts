7342a14dbaa6b16207212b97cda0bd2eb55a29c2
fedeval <federico.valenza5@gmail.com>
2021-06-06 12:11:53 +0200

    feat: add api endpoints controller and router for products

---
+++ src/controllers/productsController.ts
@@ -0,0 +1,55 @@
+import { Application, Request, Response } from 'express';
+import { Product, ProductStore } from '../models/product';
+
+const store = new ProductStore();
+
+const index = async (req: Request, res: Response) => {
+  try {
+    const products = await store.index();
+    res.json(products);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const show = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+    const product = await store.show(id);
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const create = async (req: Request, res: Response) => {
+  try {
+    const productInfo: Product = {
+      name: req.body.name,
+      price: req.body.price,
+      category: req.body.category,
+      rating: req.body.rating
+    };
+    const product = await store.create(productInfo);
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const destroy = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+    const product = await store.delete(id);
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+export const productRouter = (app: Application): void => {
+  app.get('/products', index);
+  app.get('/products/:id', show);
+  app.post('/products', create);
+  app.delete('/products/:id', destroy);
+};
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { productRouter } from './controllers/productsController';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,6 +12,9 @@ app.use(express.json());
 app.get('/', function (req: Request, res: Response) {
   res.send('Hello World!');
 });
+
+// Initialise all the routers
+productRouter(app);
 
 app.listen(PORT, function () {
   console.log(`starting app on port ${PORT}`);
