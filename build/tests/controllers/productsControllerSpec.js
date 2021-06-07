73ffb295e19ff13b58a269880f7eac7dc1c29e6b
fedeval <federico.valenza5@gmail.com>
2021-06-07 15:54:41 +0200

    test: tests for all /products endpoints

--- build/tests/controllers/productsControllerSpec.js
+++ build/tests/controllers/productsControllerSpec.js
@@ -5,11 +5,57 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("../../server"));
 const supertest_1 = __importDefault(require("supertest"));
+const database_1 = __importDefault(require("../../database"));
 const request = supertest_1.default(server_1.default);
 describe('Products controller', () => {
-    it('gets /products endpoint', async (done) => {
+    it('posts on /products/:id endpoint and returns a product in JSON format', async () => {
+        const response = await request.post('/products').send({ name: 'bike', price: 200, category: 'sports', rating: 4.32 });
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            id: 1,
+            name: 'bike',
+            price: 200,
+            category: 'sports',
+            rating: 4.32
+        });
+    });
+    it('gets /products endpoint and returns a list of products in JSON format', async () => {
         const response = await request.get('/products');
         expect(response.status).toBe(200);
-        done();
+        expect(response.body).toEqual([{
+                id: 1,
+                name: 'bike',
+                price: 200,
+                category: 'sports',
+                rating: 4.32
+            }]);
+    });
+    it('gets /products/:id endpoint and returns a product in JSON format', async () => {
+        const response = await request.get('/products/1');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            id: 1,
+            name: 'bike',
+            price: 200,
+            category: 'sports',
+            rating: 4.32
+        });
+    });
+    it('deletes on /products/:id endpoint and returns the deleted product in JSON format', async () => {
+        const response = await request.get('/products/1');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            id: 1,
+            name: 'bike',
+            price: 200,
+            category: 'sports',
+            rating: 4.32
+        });
+    });
+    afterAll(async () => {
+        const connection = await database_1.default.connect();
+        await connection.query('DELETE FROM products;');
+        await connection.query('ALTER SEQUENCE products_id_seq RESTART WITH 1;');
+        connection.release();
     });
 });
--- src/tests/controllers/productsControllerSpec.ts
+++ src/tests/controllers/productsControllerSpec.ts
@@ -1,12 +1,66 @@
 import app from '../../server';
 import supertest from 'supertest';
+import Client from '../../database';
 
 const request = supertest(app);
 
 describe('Products controller', () => {
-  it('gets /products endpoint', async (done) => {
+  it('posts on /products/:id endpoint and returns a product in JSON format', async () => {
+    const response = await request
+      .post('/products')
+      .send({ name: 'bike', price: 200, category: 'sports', rating: 4.32 });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: 1,
+      name: 'bike',
+      price: 200,
+      category: 'sports',
+      rating: 4.32
+    });
+  });
+
+  it('gets /products endpoint and returns a list of products in JSON format', async () => {
     const response = await request.get('/products');
     expect(response.status).toBe(200);
-    done();
+    expect(response.body).toEqual([
+      {
+        id: 1,
+        name: 'bike',
+        price: 200,
+        category: 'sports',
+        rating: 4.32
+      }
+    ]);
+  });
+
+  it('gets /products/:id endpoint and returns a product in JSON format', async () => {
+    const response = await request.get('/products/1');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: 1,
+      name: 'bike',
+      price: 200,
+      category: 'sports',
+      rating: 4.32
+    });
+  });
+
+  it('deletes on /products/:id endpoint and returns the deleted product in JSON format', async () => {
+    const response = await request.get('/products/1');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: 1,
+      name: 'bike',
+      price: 200,
+      category: 'sports',
+      rating: 4.32
+    });
+  });
+
+  afterAll(async () => {
+    const connection = await Client.connect();
+    await connection.query('DELETE FROM products;');
+    await connection.query('ALTER SEQUENCE products_id_seq RESTART WITH 1;');
+    connection.release();
   });
 });
